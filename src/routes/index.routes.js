05e05655cea2c4cefa676f09e096b04258b12a0a
Moises Roberto Sanchez Perez <moisesrobertosanchezperez@gmail.com>
2024-08-10 20:54:17 -0600

    configuración 3

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -12,10 +12,28 @@ let corsOptions = {
 
 const router = Router();
 
+router.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:3000',
+      'http://localhost:5173'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
 router.get('/markerkey', (req, res) => {
   try {
     res.status(200)
-      .header('Access-Control-Allow-Origin', 'http://localhost:3000')
+      // .header('Access-Control-Allow-Origin', 'http://localhost:3000')
       .json({ key: 'AIzaSyBI-iOQt72TarcbqzCHqbaGROXeqE9CPHc' })
   } catch (err) {
     res.status(404).json({ message: "No tienes acceso a este sitio" })
