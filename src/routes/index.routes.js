354643ae267de4e4f5703b2e2e4661f3d400dce7
Moises Roberto Sanchez Perez <moisesrobertosanchezperez@gmail.com>
2024-08-10 22:38:19 -0600

    error

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -24,16 +24,12 @@ router.use(cors({
 router.get('/markerkey', (req, res) => {
   try {
     res.status(200)
-      // .header('Access-Control-Allow-Origin', 'http://localhost:3000')
+      .header('Access-Control-Allow-Origin', 'http://localhost:3000')
       .json({ key: 'AIzaSyAIUZnLq5j2RqmQOvN8S2AMl3100J-npSA' })
   } catch (err) {
     res.status(404).json({ message: "No tienes acceso a este sitio" })
     console.error({ message: "No tienes acceso a este sitio" })
   }
-
-  console.log(process.env)
-
-
 })
 
 export default router;
