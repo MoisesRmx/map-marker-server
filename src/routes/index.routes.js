01c62f38d7702a2fe8d1e2c71dc6698fa87e18e4
Moises Roberto Sanchez Perez <moisesrobertosanchezperez@gmail.com>
2024-08-10 22:14:52 -0600

    Configurando más cositas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,14 +1,5 @@
 import { Router } from 'express';
 import cors from 'cors'
-import path from 'path'
-
-/*
-let corsOptions = {
-  "origin": "*",
-  "methods": "GET",
-  "optionsSuccessStatus": 200
-}
-*/
 
 const router = Router();
 
@@ -33,13 +24,13 @@ router.use(cors({
 router.get('/markerkey', (req, res) => {
   try {
     res.status(200)
-      // .header('Access-Control-Allow-Origin', 'http://localhost:3000')
-      .json({ key: 'AIzaSyBI-iOQt72TarcbqzCHqbaGROXeqE9CPHc' })
+      .json({ key: process.env.API_KEY })
   } catch (err) {
     res.status(404).json({ message: "No tienes acceso a este sitio" })
     console.error({ message: "No tienes acceso a este sitio" })
   }
 
+  console.log(process.env)
 
 
 })
